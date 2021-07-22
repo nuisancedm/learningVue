cc2904ec9382b4fdc9d9607915fdcef29cba520a
nuisancedm <85547644+nuisancedm@users.noreply.github.com>
2021-07-22 15:09:20 +0800

    Delete src directory

--- src/components/button/index.js
+++
@@ -1,5 +0,0 @@
-import mybtn from './index.vue'
-mybtn.install=function(Vue){
-    Vue.component(mybtn.name,mybtn)
-};
-export default mybtn
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import mybtn from './components/button'
-
-Vue.config.productionTip = false
-Vue.use(mybtn)
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
